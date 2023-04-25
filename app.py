980497659c583e744078138e208a01bebcd9fa98
Hans <ccoccc14@gmail.com>
2023-04-25 18:10:41 +0800

    modified:   app.py 	modified:   example_setting.json 	renamed:    dist/script.js -> static/script.js 	renamed:    dist/style.css -> static/style.css 	renamed:    dist/index.html -> templates/index.html

--- app.py
+++ app.py
@@ -10,6 +10,41 @@ app.config["SECRET_KEY"] = "mysecret"
 
 @app.route("/")
 def home():
-    return "awa"
+    return render_template("index.html")
     
+
+@app.route("/login")
+def login():
+    url=config["oauth2"]["url"]
+    id=url=config["oauth2"]["id"]
+    return redirect(f"https://discord.com/api/oauth2/authorize?client_id={id}&redirect_uri={url}%2Foauth%2Fcallback&response_type=code&scope=identify%20email")
+
+
+@app.route("/logout")
+def logout():
+    session.pop("access_token")
+    return redirect("/")
+
+
+@app.route("/oauth/callback")
+def oauth_callback():
+    code = request.args["code"]
+    url=config["oauth2"]["url"]
+    access_token = client.oauth.get_access_token(
+        code, redirect_uri=f"{url}/oauth/callback"
+    ).access_token
+    session["access_token"] = access_token
+    return redirect("/log")
+
+@app.route("/log")
+def log():
+    access_token = session.get("access_token")
+
+    if not access_token:
+        return redirect("/")
+
+    bearer_client = APIClient(access_token, bearer=True)
+    current_user = bearer_client.users.get_current_user()
+    return redirect("/")
+
 app.run(host=config["boardmate"]["host"],port=config["boardmate"]["port"],debug=True)
