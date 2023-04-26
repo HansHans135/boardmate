b6dca95f3574b110fdbef6254a078cc79cf9ddc5
Hans <ccoccc14@gmail.com>
2023-04-26 23:38:00 +0800

    modified:   .gitignore 	modified:   app.py 	modified:   data/user.json 	modified:   example_setting.json 	new file:   templates/add.html 	modified:   templates/index.html

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template, request, redirect, session,jsonify
+from flask import Flask, render_template, request, redirect, session, jsonify
 from zenora import APIClient
 import json
 import pteropy
 app = Flask(__name__)
 
-with open ("./setting.json","r")as f:
-    config=json.load(f)
-client = APIClient(config["oauth"]["bot_token"], client_secret=config["oauth"]["client_secret"])
+with open("./setting.json", "r")as f:
+    config = json.load(f)
+client = APIClient(config["oauth"]["bot_token"],
+                   client_secret=config["oauth"]["client_secret"])
 app.config["SECRET_KEY"] = "mysecret"
+
 
 @app.route("/")
 def home():
@@ -18,14 +20,56 @@ def home():
 
     bearer_client = APIClient(access_token, bearer=True)
     current_user = bearer_client.users.get_current_user()
-    return render_template("index.html")
-    
-    
+
+    with open("data/user.json", "r")as f:
+        data = json.load(f)
+    try:
+        resource = data[str(current_user.id)]["resource"]
+    except:
+        data[str(current_user.id)] = {
+            "money": 0,
+            "resource": {
+                "memory": 0,
+                "disk": 0,
+                "cpu": 0,
+                "servers": 0
+            }
+        }
+        with open("data/user.json", "w+")as f:
+            json.dump(data,f)
+        with open("data/user.json", "r")as f:
+            data = json.load(f)
+        resource = data[str(current_user.id)]["resource"]
+    resource={
+            "memory": resource["memory"]+config["server"]["default_resource"]["memory"],
+            "disk": resource["disk"]+config["server"]["default_resource"]["disk"],
+            "cpu": resource["cpu"]+config["server"]["default_resource"]["cpu"],
+            "servers":resource["servers"]+config["server"]["default_resource"]["servers"]
+        }
+    return render_template("index.html",resource=resource,user=current_user)
+
+@app.route("/server/add")
+def add():
+    access_token = session.get("access_token")
+
+    if not access_token:
+        return render_template("login.html")
+
+    bearer_client = APIClient(access_token, bearer=True)
+    current_user = bearer_client.users.get_current_user()
+
+    with open("data/user.json", "r")as f:
+        data = json.load(f)
+    try:
+        resource = data[str(current_user.id)]["resource"]
+    except:
+        return redirect("/")
+    return render_template("add.html",resource=resource,user=current_user)
 
 @app.route("/login")
 def login():
-    url=config["oauth"]["url"]
-    id=config["oauth"]["id"]
+    url = config["oauth"]["url"]
+    id = config["oauth"]["id"]
     return redirect(f"https://discord.com/api/oauth2/authorize?client_id={id}&redirect_uri={url}oauth/callback&response_type=code&scope=identify%20guilds%20email")
 
 
@@ -38,12 +82,13 @@ def logout():
 @app.route("/oauth/callback")
 def oauth_callback():
     code = request.args["code"]
-    url=config["oauth"]["url"]
+    url = config["oauth"]["url"]
     access_token = client.oauth.get_access_token(
-        code, redirect_uri=f"{url}/oauth/callback"
+        code, redirect_uri=f"{url}oauth/callback"
     ).access_token
     session["access_token"] = access_token
     return redirect("/log")
+
 
 @app.route("/log")
 def log():
@@ -56,4 +101,6 @@ def log():
     current_user = bearer_client.users.get_current_user()
     return redirect("/")
 
-app.run(host=config["boardmate"]["host"],port=config["boardmate"]["port"],debug=True)
+
+app.run(host=config["boardmate"]["host"],
+        port=config["boardmate"]["port"], debug=True)
