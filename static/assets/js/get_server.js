adffc90ff3aec32e1fe0f87b0374ac90efbb27b0
Hans <ccoccc14@gmail.com>
2025-05-15 15:14:15 +0800

    改進圖標

--- static/assets/js/get_server.js
+++ static/assets/js/get_server.js
@@ -60,21 +60,24 @@ fetch(url)
             const actionCell = document.createElement('td');
             actionCell.classList.add('px-4', 'py-3', 'text-xs');
             actionCell.innerHTML = `
-                      <a href='${server.url}' ,target='_blank'>
-                        <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                          查看
-                        </span>
-                      </a>
-                      <a href='/server/edit/${server.id}'>
-                        <span class="px-2 py-1 font-semibold leading-tight text-gray-700 bg-gray-100 rounded-full dark:text-gray-100 dark:bg-gray-700">
-                          編輯
-                        </span>
-                      </a>
-                      <a href='/server/del/${server.id}'>
-                        <span class="px-2 py-1 font-semibold leading-tight text-red-700 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-700">
-                          刪除
-                        </span>
-                      </a>
+                      <div class="flex items-center space-x-4">
+                        <a href='${server.url}' target='_blank' class="text-green-500 hover:text-green-700 dark:text-green-300 dark:hover:text-green-100" title="查看">
+                          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                            <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"></path>
+                          </svg>
+                        </a>
+                        <a href='/server/edit/${server.id}' class="text-blue-500 hover:text-blue-700 dark:text-blue-300 dark:hover:text-blue-100" title="編輯">
+                          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"></path>
+                          </svg>
+                        </a>
+                        <a href='/server/del/${server.id}' class="text-red-500 hover:text-red-700 dark:text-red-300 dark:hover:text-red-100" title="刪除">
+                          <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                          </svg>
+                        </a>
+                      </div>
                     `;
             row.appendChild(actionCell);
             serverTableBody.appendChild(row);
